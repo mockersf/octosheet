e9753644a95310159abe768f851d0db1f5767281
François Mockers <mockersf@gmail.com>
2021-05-08 00:24:44 +0200

    metadata, license and ci

--- tests/parse_musicxml_xml.rs
+++ tests/parse_musicxml_xml.rs
@@ -1,8 +1,11 @@
 use octosheet::*;
 
-use test_generator::test_resources;
+#[test]
+fn can_parse_file_ode_to_joy() {
+    assert!(dbg!(parse_file("sheets/Ode_to_joy_Piano_for_kids.xml")).is_ok());
+}
 
-#[test_resources("sheets/*.xml")]
-fn can_parse_file(file: &str) {
-    assert!(dbg!(parse_file(&file)).is_ok());
+#[test]
+fn can_parse_file_jurassic_park() {
+    assert!(dbg!(parse_file("sheets/John_Williams_-_Jurassic_Park.xml")).is_ok());
 }
